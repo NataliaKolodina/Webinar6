dc12d80f73219988c4a0f1e9674fa7560ddf57a3
«Natalia <kolodina_melbyk@mail.ru>
2023-02-16 14:56:27 +0400

    Add task3 BinarySystem

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.Write($"{num} -> {BinarySystem(num)}");
+
+string BinarySystem(int number)
+{
+    string result = "";
+    while (number > 0)
+    {
+        result = Convert.ToString(number % 2) + result;
+        number = number / 2;
+    }
+    return result;
+}
