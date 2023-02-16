7ae6dd6d6104722d34d3bbe79fba8b920cc6491c
«Natalia <kolodina_melbyk@mail.ru>
2023-02-16 15:51:37 +0400

    Add task5 Copy Array

---
+++ task5/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+
+Console.Clear();
+
+int length = InputNumber("Введите количество элементов в массиве: ");
+int min = InputNumber("Введите минимальное значение в массиве: ");
+int max = InputNumber("Введите максимальное значение в массиве: ");
+int[] arr = GetArray(length, min, max);
+Console.WriteLine(String.Join(" ", arr));
+int[] newArr = CopyArray(arr);
+Console.WriteLine(String.Join(" ", newArr));
+
+int InputNumber(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine()!);
+    return res;
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] res = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        res[i] = array[i];
+    }
+    return res;
+}
